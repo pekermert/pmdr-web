be7e059843a1bc56e2cb359c872a23f5bfbcc923
Peker Mert <pekermert@email.com>
2015-05-04 16:33:34 +0300

    wiringpi

--- index.js
+++ index.js
@@ -38,33 +38,15 @@ io.on('connection', function(socket){
 });
 
 ///RASPBERRY GPIO///
- 
-var Gpio = require('onoff').Gpio, // Constructor function for Gpio objects. 
-  led = new Gpio(14, 'out');      // Export GPIO #14 as an output. 
- 
-// Toggle the state of the LED on GPIO #14 every 200ms 'count' times. 
-// Here asynchronous methods are used. Synchronous methods are also available. 
-(function blink(count) {
-  if (count <= 0) {
-    return led.unexport();
-  }
- 
-  led.read(function (err, value) { // Asynchronous read. 
-    if (err) {
-      throw err;
-    }
- 
-    led.write(value ^ 1, function (err) { // Asynchronous write. 
-      if (err) {
-        throw err;
-      }
-    });
-  });
- 
-  setTimeout(function () {
-    blink(count - 1);
-  }, 200);
-}(25));
+var wpi = require('wiring-pi');
+wpi.setup();
+var pin = 4; //Change pin number according to your wiring
+wpi.pinMode(pin, wpi.OUTPUT);
+var value = 1;
+setInterval(function() {
+  wpi.digitalWrite(pin, value);
+  value = +!value;
+}, 500);
 /////////// HTTP/////////////
 http.listen(3300, function(){
   console.log('listening on *:3300');
