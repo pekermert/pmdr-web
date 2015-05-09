15c4f40d23c7e5aa4c67f2374a8244d38798d7c5
Peker Mert <pekermert@email.com>
2015-05-10 01:11:09 +0300

    pin int casting

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var gpio = require('rpi-gpio');
 
 // USER ID : GPIO PIN NUMBER
 var PinArray={1:7,7:11,6:13};
-
+var pinNum;
 // EXPRESS SET
 app.use(express.static(__dirname));
 app.use(cors());
@@ -28,18 +28,21 @@ io.on('connection', function(socket){
 
   socket.on('start-timer', function(time,user){
     socket.broadcast.emit('startTimer', {'time_type':time,'username':user.username,'id':user.id});
-    console.log('test'+PinArray[user.id]);
-    gpio.setup(PinArray[user.id],gpio.DIR_OUT, function(){led_on(PinArray[user.id]);});
+    console.log('test '+parseInt(PinArray[user.id]));
+    pinNum = parseInt(PinArray[user.id]);
+    gpio.setup(pinNum,gpio.DIR_OUT, function(){led_on(pinNum);});
   });
 
   socket.on('reset-timer', function(user){
     socket.broadcast.emit('resetTimer', user);
-    gpio.setup(PinArray[user.id],gpio.DIR_OUT, function(){led_off(PinArray[user.id])});
+    pinNum = parseInt(PinArray[user.id]);
+    gpio.setup(pinNum,gpio.DIR_OUT, function(){led_off(pinNum)});
   });
 
   socket.on('logout', function (data){
     socket.broadcast.emit('out-user',data);
-    gpio.setup(PinArray[data],gpio.DIR_OUT, function(){led_off(PinArray[data])});
+    pinNum = parseInt(PinArray[data]);
+    gpio.setup(pinNum,gpio.DIR_OUT, function(){led_off(pinNum)});
   });
 
 });
